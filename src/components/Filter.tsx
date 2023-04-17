103d082b2a4f3a3491037e915a5b54d9a607ea88
Viktor Ciolkowski <wikcioo@gmail.com>
2023-04-18 01:39:28 +0200

    Create Filter component with PokemonTypeList inside

---
+++ src/components/Filter.tsx
@@ -0,0 +1,19 @@
+import { Accordion, Box } from "@chakra-ui/react";
+import PokemonTypeList from "./PokemonTypeList";
+import { Type } from "../models/Type";
+
+interface Props {
+  onSelectType: (type: Type) => void;
+}
+
+const Filter = ({ onSelectType }: Props) => {
+  return (
+    <>
+      <Accordion allowMultiple>
+        <PokemonTypeList onSelectType={onSelectType} />
+      </Accordion>
+    </>
+  );
+};
+
+export default Filter;
